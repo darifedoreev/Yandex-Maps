ba2b6a59aebc0653e81e2a7e7487c3915c14b524
darifedoreev <98749377+darifedoreev@users.noreply.github.com>
2022-01-31 20:43:23 +0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,65 +1,65 @@
+import os
+import sys
+import pygame
 import requests
+from yandex_functions import get_ll_spn, get_coordinates, get_nearest_object, geocode
+
+toponym_to_find = "Красноярск, ул. Ленина, 114"
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+map_params = {
+    "ll": get_ll_spn(toponym_to_find)[0],
+    "spn": get_ll_spn(toponym_to_find)[1],
+    "l": "map"
+}
 
 
-def geocode(address):
-    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/"
-    geocoder_params = {
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": address,
-        "format": "json"}
-    response = requests.get(geocoder_request, params=geocoder_params)
-    if response:
-        json_response = response.json()
-    else:
-        raise RuntimeError(
-            f"""Ошибка выполнения запроса:
-            {geocoder_request}
-            Http статус: {response.status_code} ({response.reason})""")
-
-    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
-    return features[0]["GeoObject"] if features else None
-
-
-def get_coordinates(address):
-    toponym = geocode(address)
-    if not toponym:
-        return None, None
-    toponym_coodrinates = toponym["Point"]["pos"]
-    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-    return float(toponym_longitude), float(toponym_lattitude)
-
-
-def get_ll_spn(address):
-    toponym = geocode(address)
-    if not toponym:
-        return None, None
-    toponym_coordinates = toponym["Point"]["pos"]
-    toponym_longitude, toponym_lattitude = toponym_coordinates.split(" ")
-    ll = ",".join([toponym_longitude, toponym_lattitude])
-    envelope = toponym['boundedBy']['Envelope']
-    l, b = envelope["lowerCorner"].split(" ")
-    r, t = envelope["upperCorner"].split(" ")
-    dx = abs(float(l) - float(r)) / 2
-    dy = abs(float(t) - float(b)) / 2
-    span = f"{dx},{dy}"
-    return ll, span
-
-
-def get_nearest_object(point, kind):
-    ll = "{0},{1}".format(point[0], point[1])
-    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/"
-    geocoder_params = {
-        "apikey": '40d1649f-0493-4b70-98ba-98533de7710b',
-        "geocode": ll,
-        "format": "json"}
-    if kind:
-        geocoder_params['kind'] = kind
-    response = requests.get(geocoder_request, params=geocoder_params)
+def load_map(server, params, name):
+    response = requests.get(server, params)
     if not response:
-        raise RuntimeError(
-            f"""Ошибка выполнения запроса:
-            {geocoder_request}
-            Http статус: {response.status_code,} ({response.reason})""")
-    json_response = response.json()
-    features = json_response["response"]["GeoObjectCollection"]["featureMember"]
-    return features[0]["GeoObject"]["name"] if features else None
+        print("Ошибка выполнения запроса:")
+        print(server + params)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    map_file = name
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+load_map(map_api_server, map_params, "map1.png")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            os.remove('map1.png')
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_0:
+                map_params = {
+                    "ll": get_ll_spn(toponym_to_find)[0],
+                    "spn": get_ll_spn(toponym_to_find)[1],
+                    "l": "sat"
+                }
+                load_map(map_api_server, map_params, "map1.png")
+            elif event.key == pygame.K_1:
+                map_params = {
+                    "ll": get_ll_spn(toponym_to_find)[0],
+                    "spn": get_ll_spn(toponym_to_find)[1],
+                    "l": "map"
+                }
+                load_map(map_api_server, map_params, "map1.png") 
+            if event.key == pygame.K_2:
+                map_params = {
+                    "ll": get_ll_spn(toponym_to_find)[0],
+                    "spn": get_ll_spn(toponym_to_find)[1],
+                    "l": "sat,trf,skl"
+                }
+                load_map(map_api_server, map_params, "map1.png")            
+                
+    screen.blit(pygame.image.load('map1.png'), (0, 0))
+    pygame.display.flip()
+    
